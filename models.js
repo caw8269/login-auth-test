6be842928e065aa51ef075e83d79ff83d3e1b947
DiamondGaming <121042320+caw8269@users.noreply.github.com>
2025-09-05 19:55:31 +0000

    set up user model

---
+++ models.js
@@ -0,0 +1,13 @@
+//module.js
+const mongoose = require("mongoose");
+
+//create user model
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true},
+    username: { type: String, required: true, unique: true},
+    password: {type: String, required: true}
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
